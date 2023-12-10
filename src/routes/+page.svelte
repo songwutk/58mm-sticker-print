e6d726eb2a88ba2669314c9be4387f97a7206b54
Songwut K <songwutk@gmail.com>
2023-12-10 12:17:47 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,96 @@
+<script>
+    import "carbon-components-svelte/css/white.css";
+    import {
+      Header,
+      HeaderNav,
+      HeaderNavItem,
+      HeaderNavMenu,
+      SkipToContent,
+      Content,
+      Grid,
+      Row,
+      Column,
+    } from "carbon-components-svelte";
+    import { DataTable, Link } from "carbon-components-svelte";
+    import Launch from "carbon-icons-svelte/lib/Launch.svelte";
+  
+    let isSideNavOpen = false;
+  </script>
+  
+  <Header company="Barcode" platformName="Print" bind:isSideNavOpen>
+    <svelte:fragment slot="skip-to-content">
+      <SkipToContent />
+    </svelte:fragment>
+    <HeaderNav>
+      <!--HeaderNavItem href="/" text="Link 1" />
+      <HeaderNavItem href="/" text="Link 2" />
+      <HeaderNavItem href="/" text="Link 3" />
+      <HeaderNavMenu text="Menu">
+        <HeaderNavItem href="/" text="Link 1" />
+        <HeaderNavItem href="/" text="Link 2" />
+        <HeaderNavItem href="/" text="Link 3" />
+      </HeaderNavMenu-->
+    </HeaderNav>
+  </Header>
+  
+  <Content>
+    <Grid>
+      <Row>
+        <Column>
+          
+
+              
+              <DataTable
+                headers={[
+                  { key: "name", value: "Name" },
+                  { key: "diz", value: "Description" },
+                  { key: "bcode", value: "Barcode" },
+                  { key: "price", value: "Price" },
+                ]}
+                rows={[
+                  {
+                    id: "1",
+                    name: "ผงซุบฟ้าไทย",
+                    diz: "40 กรัม",
+                    bcode: "30001",
+                    price: "14.00",
+                  },
+                  {
+                    id: "2",
+                    name: "ผงซุบฟ้าไทย",
+                    diz: "โหล 12 ชิ้น",
+                    bcode: "30002",
+                    price: "140.00",
+                  },
+                  {
+                    id: "3",
+                    name: "ซีอิ๊วขาวสูตร 1 ",
+                    diz: "เด็กสมบูรณ์",
+                    bcode: "30003",
+                    price: "35.00",
+                  },
+                ]}
+              >
+
+                <svelte:fragment slot="cell" let:row let:cell>
+                  {#if cell.key === "name" }
+                    <Link
+                      icon={Launch}
+                      href="https://en.wikipedia.org/wiki/Round-robin_DNS"
+                      target="_blank">{cell.value}</Link
+                    >
+                  {:else}
+                    {cell.value}
+                  {/if}
+                </svelte:fragment>
+              </DataTable>
+              
+            
+
+
+
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
+  
---
+++ src/routes/bcode/+page.svelte
@@ -0,0 +1,51 @@
+<script>import Barcode from 'svelte-barcode';</script>
+
+<style>
+/* +page.svelte */
+/* print in media 58mm receipt printer */
+/* height of sticker is 20mm */
+
+.content {
+  width: 58mm;
+  height: 20mm;
+  margin: 0;
+  padding: 0;
+  font-size: 10px;
+  font-family: "THSarabunNew", sans-serif;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: center;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid #dee2e6;
+  border-radius: 0.25rem;
+}
+.msg{
+  /* margin-top : -10px; */
+  margin-top: -10px;  
+}
+</style>
+
+<!-- print code128 barcode -->
+<!-- example data 
+  [{"name":"ผงปรุงรสฟ้าไทย","diz":"200 กรัม","barcode":"323001","price","25.00"}]-->
+<div class="content"><!-- print barcode128 value is 323001 --> 
+  <Barcode
+  value={'323001'}
+  elementTag={'canvas'}
+  options={{
+    format: 'CODE128',
+    width: 2,
+    height: 20,
+    text: '323001',
+    textAlign: 'center',
+    textPosition: 'bottom',
+    fontSize: 8,
+    background: '#ffffff',
+    lineColor: '#000000',
+  }}
+  />
+  <div class="msg">ผงปรุงรสฟ้าไทย 200 กรัม</div>
+  <div >25.00 บาท</div>
+</div>
